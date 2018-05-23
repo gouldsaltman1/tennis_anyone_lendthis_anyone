285a0b76a99be5201b45b4bdb911e58f45407217
Raghu Betina <raghu@firstdraft.com>
2018-05-23 15:28:24 +0000

    Generate borrowers resource with devise

---
+++ app/controllers/borrowers_controller.rb
@@ -0,0 +1,9 @@
+class BorrowersController < ApplicationController
+  def index
+    @borrowers = Borrower.all
+  end
+
+  def show
+    @borrower = Borrower.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :borrowers
+  # Routes for the Borrower resource:
+  # READ
+  get "/borrowers", :controller => "borrowers", :action => "index"
+  get "/borrowers/:id", :controller => "borrowers", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
