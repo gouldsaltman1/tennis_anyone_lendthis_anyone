d9490296cfe7603dee6df975dd7366b586e606e8
Raghu Betina <raghu@firstdraft.com>
2018-05-23 15:29:19 +0000

    Whitelist columns in EquipmentComment for ActiveAdmin

--- app/admin/equipment_comments.rb
+++ app/admin/equipment_comments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register EquipmentComment do
+
+ permit_params :equipment_id, :borrower_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
