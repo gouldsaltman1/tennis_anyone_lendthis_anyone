c806ab94a1c95dc38001a9fe792165ddc2fc0cf9
Raghu Betina <raghu@firstdraft.com>
2018-05-23 15:29:23 +0000

    Whitelist columns in LenderComment for ActiveAdmin

--- app/admin/lender_comments.rb
+++ app/admin/lender_comments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register LenderComment do
+
+ permit_params :lender_id, :borrower_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
