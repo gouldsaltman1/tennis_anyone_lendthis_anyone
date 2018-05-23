18bfd22c7d6b5445da8b3c76e880ca1034b1c712
Raghu Betina <raghu@firstdraft.com>
2018-05-23 15:29:15 +0000

    Whitelist columns in Equipment for ActiveAdmin

--- app/admin/equipment.rb
+++ app/admin/equipment.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Equipment do
+
+ permit_params :user_id, :lender_id, :category, :price, :image, :time_available_start, :time_available_end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
